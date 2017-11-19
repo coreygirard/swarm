b81ac649e15a6e0374dd2e3fe279c0d601e5b1f6
Corey Girard <corey.r.girard@gmail.com>
2017-11-18 19:51:15 -0500

    Able to fetch names of agents and subagents in program

--- swarm.py
+++ swarm.py
@@ -1,7 +1,23 @@
 import sys
+from pprint import pprint
 from loadfile import loadfile
 from tokenize import tokenize
 import treeify
+
+def getAgentList(p):
+    temp = []
+    for agent in p:
+        if [i.tag for i in agent.code] == ['define','variable',':']:
+            for subagent in agent.children:
+                temp.append(agent.code[1].value+'.'+subagent.code[0].value)
+        else:
+            temp.append(agent.code[1].value+'.run')
+    for e in temp:
+        if e.endswith('.run'):
+            temp.append(e[:-4])
+    return sorted(temp)
+            
+    #return [e.code[1].value for e in p]
 
 # load file and pre-process
 #lines = loadfile(sys.argv[1])
@@ -12,5 +28,7 @@ lines = tokenize(lines)
 
 program = treeify.nest(lines)
 
-print(program)
+for l in program:
+    assert(l.code[0].value == 'define')
 
+pprint(getAgentList(program))
