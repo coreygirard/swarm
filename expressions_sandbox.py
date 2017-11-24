e6a811cd62f0aced7fe03607833af88e23ff59c4
Corey Girard <corey.r.girard@gmail.com>
2017-11-24 12:20:07 -0500

    Working towards exponentiation

--- expressions_sandbox.py
+++ expressions_sandbox.py
@@ -45,6 +45,17 @@ class ExpressionMult(object):
     def __repr__(self):
         return 'ExpressionMult(' + str(self.inputs) + ')'
 
+class ExpressionExponent(object):
+    def __init__(self,a,b):
+        self.a = a
+        self.b = b
+        
+    def exe(self):
+        return self.a.exe() ** self.b.exe()
+
+    def __repr__(self):
+        return 'ExpressionExponent(' + str(self.a) + '**' + str(self.b) + ')'
+
 class Variable(object):
     def __init__(self,code):
         self.code = code
@@ -56,10 +67,7 @@ def buildLiteralsAndVariables(e):
     #e = re.split(r'([a-zA-Z0-9]+)',e)
     temp = []
     for i in e:
-        if re.fullmatch(r'[-]([0-9]+)',i):
-            print('matched')
-            temp.append(Literal(-1*int(i))) # TODO: handle non-ints
-        elif re.fullmatch(r'([0-9]+)',i):
+        if re.fullmatch(r'([0-9]+)',i):
             temp.append(Literal(int(i))) # TODO: handle non-ints
         elif re.fullmatch(r'([a-zA-Z0-9]+)',i):
             temp.append(Variable(i))
@@ -101,6 +109,8 @@ def collapseParentheses(e):
     return recurse(e)
 
 def recurse(e):
+    #print(e)
+    
     if len(e) == 1:
         return e[0]
     
@@ -118,6 +128,12 @@ def recurse(e):
         return collapse(e,['+','-'],ExpressionAdd)
     elif '*' in e or '/' in e:
         return collapse(e,['*','/'],ExpressionMult)
+    elif '**' in e:
+        indices = [i for i,x in enumerate(e) if x == '**']
+        for i in reversed(indices): # go right to left to avoid disturbing future indices
+            e[i-1] = ExpressionExponent(e[i-1],e[i+1])
+            del e[i:i+2]
+        return recurse(e)
     else:
         raise Exception("invalid expression")
         
@@ -132,12 +148,13 @@ def buildExpression(e):
 
 def driver(expr):
     for e in expr:
-        print(e)
         t = buildExpression(e)
         #print(t)
-        print(t.exe())
-        print(t.exe() == eval(e))
-        print('\n')
+        if t.exe() != eval(e):
+            print(e)
+            print('Result: ' + str(t.exe()))
+            print('Should be: ' + str(eval(e)))
+            print('\n')
 
 #print(buildExpression('((3+4)*2)*     2 -7**4 - 54'))
 e = [
@@ -147,8 +164,13 @@ e = [
 '-3-4',
 '-3+53-3-3+10',
 '3+(2-1)',
-#'((3+4)*2)*     2 -7**4 - 54',
+'((3+4)*2)*     2 -7**4**2 - 54',
+'((3+4)*2)*     2 -7**4 - 54',
 '((3+4)*2)*     2 -74 - 54',
+'(2**3)**4',
+'-3**4',
+'-3**3',
+'(-3)**4',
 '(1)',
 '2+1',
 '2+1-3',
